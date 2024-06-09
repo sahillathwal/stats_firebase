3c2962c7736979c564a849990f38961e8a33d874
sahillathwal <sahillathwal566@gmail.com>
2024-06-09 14:14:11 +0530

    Showing stats in HomeView

--- lib/scoped_models/home_view_model.dart
+++ lib/scoped_models/home_view_model.dart
@@ -1,3 +1,24 @@
+import 'package:skeleton_watcher/models/stats.dart';
+import 'package:skeleton_watcher/service_locator.dart';
+import 'package:skeleton_watcher/services/firebase_service.dart';
+
 import 'base_model.dart';
+
 class HomeViewModel extends BaseModel {
+  final FirebaseService _firebaseService = locator<FirebaseService>();
+  Stats? appStats;
+
+  HomeViewModel() {
+    _firebaseService.appStats.listen(_onStatsUpdated);
+  }
+
+  void _onStatsUpdated(Stats? stats) {
+    appStats = stats; // Set the stats for the UI
+    if (stats == null) {
+      setState(ViewState.busy); // If null indicate we're still fetching
+    } else {
+      setState(ViewState
+          .dataFetched); // When not null indicate that the data is fetched
+    }
+  }
 }
